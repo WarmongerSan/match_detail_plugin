2c44335e90050bacdab81183dcf325f2fd4b35bd
Ainsley Wheeler <ainsleywheeler@outlook.com>
2015-08-09 21:31:38 +0200

    :):):):):):):):):)::):))

--- match.php
+++ match.php
@@ -114,7 +114,7 @@ switch($participant->queueType){
 }
 		?>
 		
-		<div class="blue-team">
+		<div class="blue-team pull-left">
 		<?php
 $i = 0;
 foreach($game->participants as $participant):
@@ -171,24 +171,33 @@ foreach($game->participants as $participant):
 		
 		if($i == 5){
 			echo "
-				</div><div class='purple-team'>
+				</div><div class='purple-team pull-right'>
 			";			
 		}
 		
 		?>
 		
 		<div class="<?php echo ($i < 5) ? 'blue' : 'purple'; ?>-player player<?php echo $i; ?>">
-			<div class="player-details">
-				<div class="pull-left"><img class="img-circle" src="<?php echo get_match_champion_path_for_id($participant->championId); ?>" /></div>
-				<div style="float:left; margin-top:10px;">
-					<span class="kda"><?php echo $participant->stats->kills . " / " . $participant->stats->deaths . " / " . $participant->stats->assists; ?></span><br/>
-					<span class="champion"><?php echo $participant->championName; ?> | <?php echo $participant->queueType; ?></span><br/>
+			<div class="player-info" style="float: <?php echo ($i < 5) ? 'left' : 'right' ; ?>;">
+				<div class="player-details">
+					<div class="pull-left"><img class="img-circle" src="<?php echo get_match_champion_path_for_id($participant->championId); ?>" /></div>
+					<div style="float:left; margin-top:10px;">
+						<span class="kda"><?php echo $participant->stats->kills . " / " . $participant->stats->deaths . " / " . $participant->stats->assists; ?></span><br/>
+						<span class="champion"><?php echo $participant->championName; ?> | <?php echo $participant->queueType; ?></span><br/>
+					</div>
+				</div>
+				<div style="clear:both;">
+					<img class="img-item" src="<?php echo get_match_item_path_for_id($participant->item0); ?>" /><img class="img-item" src="<?php echo get_match_item_path_for_id($participant->item1); ?>" /><img class="img-item" src="<?php echo get_match_item_path_for_id($participant->item2); ?>" /><img class="img-item" src="<?php echo get_match_item_path_for_id($participant->item3); ?>" /><img class="img-item" src="<?php echo get_match_item_path_for_id($participant->item4); ?>" /><img class="img-item" src="<?php echo get_match_item_path_for_id($participant->item5); ?>" /><img class="img-item" src="<?php echo get_match_item_path_for_id($participant->item6); ?>" /><img class="img-spell" src="<?php echo get_match_spell_path_for_id($participant->spell1); ?>" /><img class="img-spell" src="<?php echo get_match_spell_path_for_id($participant->spell2); ?>" />
 				</div>
 			</div>
-			<div style="clear:both;">
-				<img class="img-item" src="<?php echo get_match_item_path_for_id($participant->item0); ?>" /><img class="img-item" src="<?php echo get_match_item_path_for_id($participant->item1); ?>" /><img class="img-item" src="<?php echo get_match_item_path_for_id($participant->item2); ?>" /><img class="img-item" src="<?php echo get_match_item_path_for_id($participant->item3); ?>" /><img class="img-item" src="<?php echo get_match_item_path_for_id($participant->item4); ?>" /><img class="img-item" src="<?php echo get_match_item_path_for_id($participant->item5); ?>" /><img class="img-item" src="<?php echo get_match_item_path_for_id($participant->item6); ?>" /><img class="img-spell" src="<?php echo get_match_spell_path_for_id($participant->spell1); ?>" /><img class="img-spell" src="<?php echo get_match_spell_path_for_id($participant->spell2); ?>" />
+			<div class="progress-info" style="float: <?php echo ($i < 5) ? 'left' : 'right' ; ?>;">
+				<div class="progress-bar" style="text-align: <?php echo ($i < 5) ? 'left' : 'right' ; ?>;">
+				  <div class="bar red" style="clear: both; width: 25% !important; <?php echo ($i < 5) ? '' : 'float: right;' ; ?>">&nbsp;</div>
+				  <div class="bar green" style="clear: both; width: 34% !important; <?php echo ($i < 5) ? '' : 'float: right;' ; ?>">&nbsp;</div>
+				  <div class="bar orange" style="clear: both; width: 44% !important; <?php echo ($i < 5) ? '' : 'float: right;' ; ?>">&nbsp;</div>
+				</div>
 			</div>
-
+			<div style="clear: both;"></div>
 		</div>
 
 		<?php
